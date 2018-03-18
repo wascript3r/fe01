574b4b27b00edd734f648afe1a7df110782dc0f3
WaScript3r <wascript3r@gmail.com>
2018-03-18 17:53:37 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+document.querySelector('#readmore1').onclick = () => {
+    alert('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nisi libero, consectetur ac tortor et, vehicula porttitor elit. Donec dapibus consectetur velit, eget feugiat ligula interdum id. Aliquam eu mattis erat. Duis non metus eros. Etiam finibus luctus euismod. Donec semper neque at fringilla vulputate. In aliquam enim et est dictum, sit amet consectetur ante elementum. Morbi a dolor tincidunt, vehicula ex at, accumsan orci. Cras in metus eu tortor fermentum rutrum. Aliquam suscipit in metus at luctus. Praesent quis lorem odio. Curabitur mi purus, cursus quis eros dictum, vulputate interdum est.');
+};
+
+document.querySelector('#readmore2').onclick = () => {
+    alert('Aliquam pretium massa odio. In eget justo metus. Donec tincidunt, libero et vulputate laoreet, justo quam convallis ipsum, a consectetur sapien neque et odio. Aenean at ullamcorper nisi. Mauris finibus, odio sit amet viverra pellentesque, metus augue convallis nisl, vel rhoncus velit velit ac urna. Suspendisse porta ligula in arcu varius, non tristique arcu efficitur. Cras cursus leo ac accumsan suscipit. Cras ultrices varius quam eget euismod. Vivamus sagittis condimentum justo, laoreet commodo purus interdum eu. Fusce scelerisque elementum enim, eget luctus elit cursus nec.');
+};
+
+document.querySelector('#readmore3').onclick = () => {
+    alert('Fusce vitae quam quis elit consequat pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque laoreet, lorem eu iaculis tincidunt, ex orci accumsan enim, eget vehicula purus sem nec urna. Quisque mattis mollis luctus. Maecenas volutpat, nulla condimentum imperdiet iaculis, mi ex scelerisque odio, vel blandit nisi lorem sit amet est. Pellentesque cursus urna eu est lobortis, vitae imperdiet magna fermentum. Praesent vitae ornare odio. Pellentesque vitae aliquam felis. Morbi quis varius urna. Vivamus vitae massa magna.');
+};
